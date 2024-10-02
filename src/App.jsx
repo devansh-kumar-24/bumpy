57b20f4296a57937b211e534019e2246c404ceeb
devansh-kumar-24 <kumardev2124@gmail.com>
2024-10-02 16:43:30 +0530

    sdfd

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen flex  flex-col bg-green-50">
+    <div className="min-h-screen flex  flex-col ">
       <Header />
       <main>
         <HeroSection />
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 import Designer from '../assets/2.png';
 
 const HeroSection = () => (
-  <section className="bg-green-50 w-[90%] mx-auto py-10">
+  <section className="bg-white w-[90%] mx-auto py-10">
     <div className="container mx-auto px-4 flex flex-col lg:flex-row items-center justify-center mb-10 lg:mb-20">
       <div className="lg:w-1/2 text-center lg:text-left mb-8 lg:mb-0">
         <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold text-black mb-4">
@@ -10,7 +10,7 @@ const HeroSection = () => (
         <p className="text-base sm:text-lg lg:text-2xl text-gray-600 mb-6 lg:mb-8">
           Your AI-Powered Map Companion for Seamless Exploration
         </p>
-        <button className="bg-green-500 text-lg sm:text-xl hover:bg-green-600 text-white font-bold py-3 sm:py-4 px-8 sm:px-10 rounded-lg transition duration-300">
+        <button className="bg-yellow-400 text-lg sm:text-xl hover:bg-yellow-500  text-white font-bold py-3 sm:py-4 px-8 sm:px-10 rounded-lg transition duration-300">
           Explore Now
         </button>
       </div>
